6093b598d296c2067e7df5aa6b806617aecf0aa3
Kerren <kerren@entrostat.com>
2018-07-22 20:06:08 +0200

    Added the user controller and service

--- backend/src/app/modules/user/user.module.ts
+++ backend/src/app/modules/user/user.module.ts
@@ -1,4 +1,9 @@
+import { UserController } from '@modules/user/controllers/user.controller';
+import { UserService } from '@modules/user/services/user.service';
 import { Module } from '@nestjs/common';
 
-@Module({})
+@Module({
+    controllers: [ UserController ],
+    providers: [ UserService ]
+})
 export class UserModule {}
