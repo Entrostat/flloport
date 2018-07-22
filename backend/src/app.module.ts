d36d720909253cde844d1ee28aafe9d6dd950caf
Kerren <kerren@entrostat.com>
2018-07-22 19:56:07 +0200

    Added paths to the tsconfig

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { SharedModule } from './shared/shared.module';
+import { UserModule } from '@modules/user/user.module';
+import { SharedModule } from '@modules/shared/shared.module';
 
 @Module({
   imports: [UserModule, SharedModule],
